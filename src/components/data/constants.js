a41f396bcc0a66b0bd7ab0913123860dca2a3369
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-08-24 22:44:55 +0300

    Add images

---
+++ src/components/data/constants.js
@@ -0,0 +1,27 @@
+export const Bio = {
+  name: "Abdulahi",
+  roles: [
+    "Full stack developer",
+    "UI/UX Designer",
+    "Web Developer",
+    "Programmer",
+  ],
+  description:
+    "a versatile creator passionate about crafting digital solutions that are as functional as they are visually engaging. I thrive on the journey of translating innovative ideas into seamless, user-centered experiences",
+  resume:
+    "https://drive.google.com/file/d/11TGQMVX8AFVdda9iQExLVNn9qyOhY55R/view?usp=sharing",
+  github: "https://github.com/Oscarj-8",
+  linkedin: "https://www.linkedin.com/in/abdulahi-muhammed-352851285/",
+};
+
+export const Skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image: "",
+      },
+    ],
+  },
+];
