07827d97c96bdd828e51d27b7ca9698c0ef4685e
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-09-23 21:34:45 +0300

    Fix llight circles

--- src/App.js
+++ src/App.js
@@ -19,87 +19,113 @@ const Body = styled.div`
   gap: 3em;
 `;
 
+// const CircleOne = styled.div`
+//   position: absolute;
+
+//   &::before {
+//     content: "";
+//     position: absolute;
+//     width: 35em;
+//     height: 35em;
+//     opacity: 50%;
+//     border-radius: 50%;
+//     top: -10em;
+//     left: -30em;
+//     background-color: #25cfa7;
+//     filter: blur(10em);
+//     z-index: 0;
+
+//     @media screen and (min-width: 500px) {
+//       opacity: 40%;
+//       left: -25em;
+//     }
+
+//     @media screen and (min-width: 1200px) {
+//       left: -15em;
+//     }
+
+//     @media screen and (min-width: 1400px) {
+//       opacity: 70%;
+//     }
+//   }
+// `;
+
+// const CircleTwo = styled.div`
+//   position: absolute;
+
+//   &::before {
+//     content: "";
+//     position: absolute;
+//     width: 35em;
+//     height: 35em;
+//     opacity: 30%;
+//     border-radius: 50%;
+//     top: 25em;
+//     right: -53em;
+//     background-color: #9925cf;
+//     filter: blur(10em);
+//     z-index: 0;
+
+//     @media screen and (min-width: 500px) {
+//       opacity: 40%;
+//       top: 25em;
+//       right: -60em;
+//     }
+
+//     @media screen and (min-width: 768px) {
+//       top: 5em;
+//       right: -70em;
+//     }
+
+//     @media screen and (min-width: 768px) {
+//       top: 5em;
+//       right: -80em;
+//     }
+
+//     @media screen and (min-width: 1200px) {
+//       top: 10em;
+//       right: -85em;
+//     }
+
+//     @media screen and (min-width: 1400px) {
+//       right: -90em;
+//     }
+
+//     @media screen and (min-width: 1600px) {
+//       right: -105em;
+//     }
+
+//     @media screen and (min-width: 1800px) {
+//       top: 15em;
+//       right: -120em;
+//     }
+//   }
+// `;
+
 const CircleOne = styled.div`
   position: absolute;
+  background-color: #25cfa7;
+  border-radius: 50%;
+  width: 60vw;
+  height: 60vw;
+  top: 0vh;
+  left: -8vw;
+  filter: blur(10em);
 
-  &::before {
-    content: "";
-    position: absolute;
-    width: 35em;
-    height: 35em;
+  @media screen and (min-width: 500px) {
     opacity: 50%;
-    border-radius: 50%;
-    top: -10em;
-    left: -30em;
-    background-color: #25cfa7;
-    filter: blur(10em);
-    z-index: 0;
-
-    @media screen and (min-width: 500px) {
-      opacity: 40%;
-      left: -25em;
-    }
-
-    @media screen and (min-width: 1200px) {
-      left: -15em;
-    }
-
-    @media screen and (min-width: 1400px) {
-      opacity: 70%;
-    }
   }
 `;
 
 const CircleTwo = styled.div`
   position: absolute;
-
-  &::before {
-    content: "";
-    position: absolute;
-    width: 35em;
-    height: 35em;
-    opacity: 30%;
-    border-radius: 50%;
-    top: 25em;
-    right: -53em;
-    background-color: #9925cf;
-    filter: blur(10em);
-    z-index: 0;
-
-    @media screen and (min-width: 500px) {
-      opacity: 40%;
-      top: 25em;
-      right: -60em;
-    }
-
-    @media screen and (min-width: 768px) {
-      top: 5em;
-      right: -70em;
-    }
-
-    @media screen and (min-width: 768px) {
-      top: 5em;
-      right: -80em;
-    }
-
-    @media screen and (min-width: 1200px) {
-      top: 10em;
-      right: -85em;
-    }
-
-    @media screen and (min-width: 1400px) {
-      right: -90em;
-    }
-
-    @media screen and (min-width: 1600px) {
-      right: -105em;
-    }
-
-    @media screen and (min-width: 1800px) {
-      top: 15em;
-      right: -120em;
-    }
-  }
+  background-color: #9925cf;
+  border-radius: 50%;
+  width: 30vw;
+  height: 80vw;
+  top: 65vh;
+  right: -15vw;
+  filter: blur(10em);
 `;
 
 function App() {
