01250fa3a53f34dbb24974e6259c6e2162ee7ea3
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-10-18 11:26:16 +0300

    Remove comments

--- server/server.js
+++ server/server.js
@@ -11,17 +11,6 @@ const emailPassword = process.env.EMAIL_PASSWORD;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Handle CORS (if your React app is on a different domain)
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Access-Control-Allow-Origin",
-//     "https://abdulahimuhammedportfolio.vercel.app"
-//   );
-//   res.setHeader("Access-Control-Allow-Methods", "OPTIONS, POST");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
-
 app.use(
   cors({
     origin: "https://abdulahimuhammedportfolio.vercel.app",
