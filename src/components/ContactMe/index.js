71c7f2666465de65a8157656253d220e0c3642e2
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-11-02 23:48:02 +0300

    Last commit

--- src/components/ContactMe/index.js
+++ src/components/ContactMe/index.js
@@ -4,6 +4,8 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useState } from "react";
 import check from "../../images/check-mark.png";
+import check2 from "../../images/checked.png";
+import check from "../../images/check-mark.png";
 const inputStyles = `
  font-family: "Montserrat", sans-serif;
   width: 100%;
